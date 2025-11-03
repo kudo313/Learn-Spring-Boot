9d4c4c28ba2f0e74cf1c7369221189e089bb8020
kudo313 <kudo3139a@gmail.com>
2025-11-03 22:31:06 +0700

    start 1

---
+++ api-gatewway/src/main/java/net/javaguides/api_gatewway/ApiGatewwayApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.api_gatewway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewwayApplication.class, args);
+	}
+
+}
